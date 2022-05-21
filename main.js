e6f99306cb784cc084750752f17f6905599b5bb9
vih0 <vitoria.s.araujo1020@gamil.com>
2022-05-21 20:25:27 -0300

    cronômetro com horario correto e atualizando a cada segundo

--- main.js
+++ main.js
@@ -2,27 +2,34 @@ const segundo = 1000;
 const minuto = 60* parseInt(segundo);
 const hora = 60* parseInt(minuto);
 const dia = 24* parseInt(hora);
-
+let total ;
+let finalData;
+let idContador;
 
 function start (){
 
     const date = document.querySelector("input").value;
 
-    let finalData = new Date(date).getTime();
-    let now = new Date().getTime();
-    let total = finalData - now;
-
-    let dias = Math.floor(total / dia);
-    let horas = Math.floor((total%dia)/hora) + 3;
-    let minutos = Math.floor((total%hora)/minuto);
-    let segundos = Math.floor((total%minuto)/segundo);
-
-    console.log(dias, horas, minutos, segundos);
-
-    document.querySelector("h1").innerHTML = ` faltam: ${dias} d ${horas} h ${minutos} m ${segundos}s`;
+    finalData = new Date(date).getTime();
+    idContador = setInterval(contador, 1000);
     
 } 
 
+function contador(){
+    let now = new Date().getTime();
+    total = finalData - now;
+    const dias = Math.floor(total / dia);
+    const horas = Math.floor((total%dia)/hora) + 3;
+    const minutos = Math.floor((total%hora)/minuto);
+    const segundos = Math.floor((total%minuto)/segundo);
+    
+    console.log(dias, horas, minutos, segundos);
+    
+    document.querySelector("h1").innerHTML = ` faltam: ${dias} d ${horas} h ${minutos} m ${segundos}s`;
+    
+    
+}
 function reset (){
     document.querySelector("h1").innerHTML = "";
+    clearInterval(idContador);
 }
