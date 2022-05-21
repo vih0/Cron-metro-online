205a930637f4421eed45fc995355dfd07d66db6b
vih0 <vitoria.s.araujo1020@gamil.com>
2022-05-21 20:05:29 -0300

    cronômetro funcionando

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
+const segundo = 1000;
+const minuto = 60* parseInt(segundo);
+const hora = 60* parseInt(minuto);
+const dia = 24* parseInt(hora);
+
+
 function start (){
+
+    const date = document.querySelector("input").value;
+
+    let finalData = new Date(date).getTime();
+    let now = new Date().getTime();
+    let total = finalData - now;
+
+    let dias = Math.floor(total / dia);
+    let horas = Math.floor((total%dia)/hora) + 3;
+    let minutos = Math.floor((total%hora)/minuto);
+    let segundos = Math.floor((total%minuto)/segundo);
+
+    console.log(dias, horas, minutos, segundos);
+
+    document.querySelector("h1").innerHTML = ` faltam: ${dias} d ${horas} h ${minutos} m ${segundos}s`;
     
+} 
+
+function reset (){
+    document.querySelector("h1").innerHTML = "";
 }
